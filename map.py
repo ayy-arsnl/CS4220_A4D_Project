24c1fd6e5cb693dd75b4a279d7fb3db253b1312a
ayy-arsnl <aurquhar@uccs.edu>
2021-10-25 15:22:49 -0600

    added function to output html file, along with the generated html file

--- map.py
+++ map.py
@@ -8,3 +8,4 @@ fig = px.scatter_mapbox(targets, lat="latitude", lon="longitude", hover_name="co
 fig.update_layout(mapbox_style="open-street-map")
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 fig.show()
+fig.write_html("map.html")
