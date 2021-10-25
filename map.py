5beaeaee03b82fe7d331903573dffb65b1e60972
ayy-arsnl <aurquhar@uccs.edu>
2021-10-24 20:38:42 -0600

    First plot with plotly and dataset

---
+++ map.py
@@ -0,0 +1,10 @@
+import pandas as pd
+targets = pd.read_csv("honeypot_data.csv")
+
+import plotly.express as px
+
+fig = px.scatter_mapbox(targets, lat="latitude", lon="longitude", hover_name="country", hover_data=["srcstr"],
+                        color_discrete_sequence=["fuchsia"], zoom=3, height=900)
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show()
