50db58af33afdbbad8443264ff14ec225e6020c9
ayy-arsnl <aurquhar@uccs.edu>
2021-10-31 13:45:54 -0600

    Added smaller testing csv as well as hover ability and color coded data points based on protocol

--- map.py
+++ map.py
@@ -1,11 +1,13 @@
 import pandas as pd
-targets = pd.read_csv("honeypot_data.csv")
+targets = pd.read_csv("honeypot_small.csv")
 
 import plotly.express as px
 
-fig = px.scatter_mapbox(targets, lat="latitude", lon="longitude", hover_name="country", hover_data=["srcstr"],
-                        color_discrete_sequence=["fuchsia"], zoom=3, height=900)
+# fig = px.scatter_mapbox(targets, lat="latitude", lon="longitude", hover_name="title", hover_data=["title", "city","state", "country"],
+#                         color_discrete_sequence=["fuchsia"], zoom=3, height=900)
+
+fig = px.scatter_mapbox(targets, lat="latitude", lon="longitude", hover_name="country", hover_data=["datetime", "srcstr", "proto"],
+                        color_continuous_scale=px.colors.cyclical.IceFire, zoom=3, height=900, color="proto")
 fig.update_layout(mapbox_style="open-street-map")
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+#fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 fig.show()
-fig.write_html("map.html")
